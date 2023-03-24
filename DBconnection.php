00eaceea0f3feaf6d9bf467927f3a9cc636a2c0f
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 05:19:41 -0700

    commit v0.4 (Login/Register Functionality Test)

--- DBconnection.php
+++ DBconnection.php
@@ -1,24 +1,15 @@
 <?php
-$con = mysqli_connect("cosc360.ok.ubc.ca","83864363","83864363","db_83864363");
-
-    echo "It worked !";
-    $username= $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    // $verifyPassword = $_POST['verifyPassword'];
-    $selectedOption = $_POST['selectionMenu'];
-    $image = file_get_contents($_FILES['img']['pfp']);
-
-    if($con->connect_error){
-        echo "Connection Failed !";
-        die('Connection Failed :' .$con->connect_error);
-    }else{
-        $stmt = $con->prepare("INSERT INTO `user_auth` (`Username`, `Email`, `Password`,`comingFrom`,`profilePicture`) VALUES (?,?,?,?,?)");
-        $stmt->bind_param("ssssb",$username,$email,$password,$selectedOption,$image);
-        $stmt->execute();
-        header('location:signUp.php');
-        $stmt->close();
-        $con->close();
-    }
-
+// Database configuration  
+$dbHost     = "cosc360.ok.ubc.ca";  
+$dbUsername = "83864363";  
+$dbPassword = "83864363";  
+$dbName     = "db_83864363";  
+  
+// Create database connection  
+$con = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);  
+  
+// Check connection  
+if ($con->connect_error) {  
+    die("Connection failed: " . $con->connect_error);
+}
 ?>
