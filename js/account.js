e0fb6ceda163ba37932bb50a02ff860e228ab8d8
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 11:27:44 -0700

    commit v0.5 (Session Management Test)

--- js/account.js
+++ js/account.js
@@ -1,12 +1,7 @@
-const email = document.getElementsById("email");
-const password = document.getElementsById("password");
-
 window.onload = function() {
+    const email = document.getElementsById("email");
+    const password = document.getElementsById("password");
     email.disabled = true;
     password.disabled = true;
 };
 
-function editField(){
-   email.disabled = false;
-   password.disabled = false;
-}
