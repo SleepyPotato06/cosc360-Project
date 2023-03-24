fc8053e980d4923e065402f6f3a1e7a3927aa229
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 10:11:04 -0700

    commit v0.5 (Session Management Test)

--- account.php
+++ account.php
@@ -3,12 +3,13 @@ session_start();
 
 $email= $_POST['email'];
 $password = $_POST['password'];
+$Id = (int) $_SESSION["Id"];
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }else{
     $stmt = $con->prepare("UPDATE `user_auth` SET `Email` = ?, `Password` = ? WHERE `Id` = ?");
-    $stmt->bind_param("ssi", $email,$password,$_SESSION["Id"]); 
+    $stmt->bind_param("ssi", $email,$password,$Id); 
     $stmt->execute();
 }
 ?>
