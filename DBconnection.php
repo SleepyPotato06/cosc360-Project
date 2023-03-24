d150ba2efc8acd66c2d60223bdaea0bdf4f99c1a
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 04:10:20 -0700

    commit v0.4 (Login/Register Functionality Test)

--- DBconnection.php
+++ DBconnection.php
@@ -7,16 +7,16 @@ $con = mysqli_connect("cosc360.ok.ubc.ca","83864363","83864363","db_83864363");
     $password = $_POST['password'];
     // $verifyPassword = $_POST['verifyPassword'];
     $selectedOption = $_POST['selectionMenu'];
-    // $image = $_POST['img'];
+    $image = $_POST['img'];
 
     if($con->connect_error){
         echo "Connection Failed !";
         die('Connection Failed :' .$con->connect_error);
     }else{
-        $stmt = $con->prepare("INSERT INTO `user_auth` (`Username`, `Email`, `Password`,`comingFrom`) VALUES (?,?,?,?)");
-        $stmt->bind_param("ssss",$username,$email,$password,$selectedOption);
+        $stmt = $con->prepare("INSERT INTO `user_auth` (`Username`, `Email`, `Password`,`comingFrom`,`profilePicture`) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("ssssb",$username,$email,$password,$selectedOption,$image);
         $stmt->execute();
-        header('location:SignUp.php');
+        header('location:signUp.php');
         $stmt->close();
         $con->close();
     }
