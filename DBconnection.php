c5b2361728054c2577e22c8a59076ebd59b3a6f2
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 05:43:24 -0700

    commit v0.4 (Login/Register Functionality Test)

--- DBconnection.php
+++ DBconnection.php
@@ -8,8 +8,4 @@ $dbName     = "db_83864363";
 // Create database connection  
 $con = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);  
   
-// Check connection  
-if ($con->connect_error) {  
-    die("Connection failed: " . $con->connect_error);
-}
 ?>
