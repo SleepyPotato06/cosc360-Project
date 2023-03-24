e6e96d03b51e3704f8d2838f65a3e05b7b2bc196
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 11:23:25 -0700

    commit v0.5 (Session Management Test)

--- account.php
+++ account.php
@@ -69,14 +69,14 @@ $Id = $_SESSION["Id"];
                     <div class="item-1">
                         <label>Email</label><br>    
                         <div class="input-container">
-                            <input type = "text" id="email" name = "email" value = "<?php echo $_SESSION["email"] ;?>" disabled="">
+                            <input type = "text" id="email" name = "email" value = "<?php echo $_SESSION["email"] ;?>">
                             <button class="editField" onclick="editField()"><img src="svgs/editField.svg"></button>
                         </div> 
                     </div>
                     <div class="item-2">
                         <label>Password</label><br>
                         <div class="input-container">
-                            <input type = "password" id="password" name = "password" disabled="disabled">
+                            <input type = "password" id="password" name = "password">
                             <button class="editField" onclick="editField()"><img src="svgs/editField.svg"></button>
                         </div>
                     </div>
--- js/account.js
+++ js/account.js
@@ -1,6 +1,7 @@
 const email = document.getElementsById("email");
 const password = document.getElementsById("password");
-
+email.disabled = true;
+password.disabled = true;
 
 function editField(){
    email.disabled = false;
