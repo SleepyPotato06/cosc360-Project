f3ed56204e72f3011ae8e5c31991959d11c22366
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 04:21:24 -0700

    commit v0.4 (Login/Register Functionality Test)

--- DBconnection.php
+++ DBconnection.php
@@ -14,7 +14,7 @@ $con = mysqli_connect("cosc360.ok.ubc.ca","83864363","83864363","db_83864363");
         die('Connection Failed :' .$con->connect_error);
     }else{
         $stmt = $con->prepare("INSERT INTO `user_auth` (`Username`, `Email`, `Password`,`comingFrom`,`profilePicture`) VALUES (?,?,?,?,?)");
-        $stmt->bind_param("sssss",$username,$email,$password,$selectedOption,$image);
+        $stmt->bind_param("ssssb",$username,$email,$password,$selectedOption,$image);
         $stmt->execute();
         header('location:signUp.php');
         $stmt->close();
